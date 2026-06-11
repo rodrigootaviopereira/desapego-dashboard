c8b5c41e7746c6a633500bc7ab5a9402c14085fc
rodrigootaviopereira <rodrigootaviopereira@gmail.com>
2026-06-11 15:09:05 -0300

    fix: adiciona basePath para GitHub Pages em subdiretorio

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   output: "export",
+  basePath: isProd ? "/desapego-dashboard" : "",
+  assetPrefix: isProd ? "/desapego-dashboard/" : "",
 };
+
 module.exports = nextConfig;
